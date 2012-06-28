1130f8f9c19f0bb0a10d124d267838e92ab12e16
Rinat <ibragimovrinat@mail.ru>
2012-06-28 19:23:27 +0400

    fill channel list and make it selectable

--- viewer.py
+++ viewer.py
@@ -12,37 +12,52 @@ class MainWindow(QMainWindow):
         self.createWidgets()
 
     def createWidgets(self):
+        # widgets
         self.dataplot = DataPlot()
         self.channel_list = QListWidget()
+        central_widget = QWidget()
 
+        # layout
         hbox = QHBoxLayout()
         hbox.addWidget(self.dataplot, stretch=1)
         hbox.addWidget(self.channel_list, stretch=0)
-
-        central_widget = QWidget()
         central_widget.setLayout(hbox)
         self.setCentralWidget(central_widget)
 
+        # properties and signals
+        self.channel_list.currentRowChanged.connect(self.channelListCurrentRowChanged)
+
     def attach_osc(self, osc):
         self.osc = osc
+        self.channel_list.reset()
+        for ch in self.osc.channel:
+            channel = self.osc.channel[ch]
+            item = QListWidgetItem(channel.name)
+            item.cid = ch
+            self.channel_list.addItem(item)
         self.plot_osc_channel(1)
 
     def plot_osc_channel(self, ch):
+        self.dataplot.detachItems()
         curve = QwtPlotCurve("Channel data")
         curve.setSymbol(QwtSymbol(QwtSymbol.XCross, QBrush(), QPen(Qt.black), QSize(7,7)))
         curve.setPen(QPen(Qt.red))
         curve.attach(self.dataplot)
         dt = 1.0 / self.osc.sample_rate
-        print self.osc.line_frequency
         t = [dt * s for s in range(0, len(self.osc.channel[ch].data))]
         curve.setData(t, self.osc.channel[ch].data)
         self.dataplot.setAxisTitle(QwtPlot.xBottom, "Time (s)")
         if self.osc.channel[ch].ctype == 'A':
             self.dataplot.setAxisTitle(QwtPlot.yLeft, self.osc.channel[ch].units)
+            self.dataplot.setAxisAutoScale(QwtPlot.yLeft)
         else:
             self.dataplot.setAxisTitle(QwtPlot.yLeft, "on/off")
             self.dataplot.setAxisScale(QwtPlot.yLeft, 0, 1)
         self.dataplot.replot()
+
+    def channelListCurrentRowChanged(self, row):
+        cid = self.channel_list.item(row).cid
+        self.plot_osc_channel(cid)
 
 if __name__ == '__main__':
     osc = OscReader('osc-examples/01340703835235278186.cfg')
