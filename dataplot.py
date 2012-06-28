4eff62313ff4ea1d803a13f7dca68e27b918ba86
Rinat <ibragimovrinat@mail.ru>
2012-06-28 21:28:32 +0400

    add zooming

--- dataplot.py
+++ dataplot.py
@@ -5,3 +5,6 @@ class DataPlot(QwtPlot):
     def __init__(self, *args):
         QwtPlot.__init__(self, *args)
         self.setCanvasBackground(Qt.white)
+        self.zoomer = QwtPlotZoomer(QwtPlot.xBottom, QwtPlot.yLeft, \
+            QwtPicker.DragSelection, QwtPicker.AlwaysOff, self.canvas())
+        self.zoomer.setRubberBandPen(QPen(Qt.green))
